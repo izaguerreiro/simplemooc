1b99bb7f65a0941c526407be6648dcf5e33e7299
Izabela Guerreiro <izaguerreiro@gmail.com>
2017-01-04 16:12:05 -0200

    logout

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from simplemooc.accounts.views import register
 
 
 urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', register, name='register'),
 ]
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.contrib.auth import authenticate, login
 from .forms import RegisterForm
 
 
@@ -10,8 +11,10 @@ def register(request):
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
 
