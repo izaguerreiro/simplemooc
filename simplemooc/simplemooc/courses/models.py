0f21c2dd5995fbe05158bb99bd94b2b8f73e3744
izaguerreiro <izaguerreiro@gmail..com>
2017-01-11 15:43:35 -0200

    fix

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -31,6 +31,10 @@ class Course(models.Model):
     def get_absolute_url(self):
         return ('courses:details', (), {'slug': self.slug})
 
+    def release_lessons(self):
+        today = timezone.now().date()
+        return self.lessons.filter(release_date__gte=today)
+
     class Meta:
         db_table = 'courses'
         verbose_name = 'curso'
@@ -49,6 +53,12 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.name
+
+    def is_available(self):
+        if self.release_date:
+            today = timezone.now().date()
+            return self.release_date >= today
+        return False
 
     class Meta:
         verbose_name = 'aula'
