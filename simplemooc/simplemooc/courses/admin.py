856b394f6d2dfc46c9364796b49430c8de09dde0
izaguerreiro <izaguerreiro@gmail..com>
2017-01-11 10:36:21 -0200

    Envia email quando um anuncio é cadastrado. Cria os models Lesson e Material

--- simplemooc/simplemooc/courses/admin.py
+++ simplemooc/simplemooc/courses/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Course, Enrollment, Announcement, Comment
+from .models import Course, Enrollment, Announcement
+from .models import Comment, Lesson, Material
 
 
 class CourseAdmin(admin.ModelAdmin):
@@ -8,5 +9,17 @@ class CourseAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class MaterialInlineAdmin(admin.TabularInline):
+    model = Material
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'number', 'course', 'release_date']
+    search_fields = ['name', 'description']
+    list_filter = ['created_at']
+    inlines = [MaterialInlineAdmin]
+
+
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register([Enrollment, Announcement, Comment])
--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -38,6 +38,44 @@ class Course(models.Model):
         ordering = ['name']
 
 
+class Lesson(models.Model):
+    name = models.CharField('nome', max_length=100)
+    description = models.TextField('descrição', blank=True)
+    number = models.IntegerField('número (ordem)', blank=True, default=0)
+    release_date = models.DateField('data de liberação', blank=True, null=True)
+    course = models.ForeignKey(Course, verbose_name='curso', related_name='lessons')
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'aula'
+        verbose_name_plural = 'aulas'
+        ordering = ['number']
+
+
+class Material(models.Model):
+    name = models.CharField('nome', max_length=100)
+    embedded = models.TextField('vídeo embedded', blank=True)
+    file = models.FileField(
+        upload_to='lessons/materials', verbose_name='arquivo',
+        blank=True, null=True
+    )
+    lesson = models.ForeignKey(Lesson, verbose_name='aula', related_name='materials')
+
+    def is_embedded(self):
+        return bool(self.embedded)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'material'
+        verbose_name_plural = 'materiais'
+
+
 class Enrollment(models.Model):
     STATUS_CHOICES = (
         (0, 'Pendente'),
@@ -104,8 +142,19 @@ class Comment(models.Model):
 
 
 def post_save_announcement(instance, created, **kwargs):
-    subject = instance.title
-    template_name = 'courses/announcement-mail.html'
-    context = {'announcement': announcement}    
-    send_mail_template(subject, template_name, context, recipient_list)
+    if created:
+        subject = instance.title
+        template_name = 'courses/announcement-mail.html'
+        context = {'announcement': instance}
+        enrollments = Enrollment.objects.filter(
+            course=instance.course, status=1
+        )
+        for enrollment in enrollments:
+            recipient_list = [enrollment.user.email]
+            send_mail_template(subject, template_name, context, recipient_list)
 
+
+models.signals.post_save.connect(
+    post_save_announcement, sender=Announcement,
+    dispatch_uid='post_save_announcement'
+)
