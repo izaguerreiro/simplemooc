747c3f2731914daf9c6842214236a98144f882ea
Izabela Guerreiro <izaguerreiro@gmail.com>
2017-01-16 01:02:14 -0200

    fix

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
 
     'simplemooc.core',
     'simplemooc.accounts',
     'simplemooc.courses',
+    'simplemooc.threads',
 ]
 
 MIDDLEWARE = [
---
+++ simplemooc/simplemooc/threads/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from taggit.managers import TaggableManager
+from django.conf import settings
+
+
+class Thread(models.Model):
+    title = models.CharField('título', max_length=100)
+    body = models.TextField('mensagem')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='autor', related_name='threads'
+    )
+    views = models.IntegerField('visualizações', blank=True, default=0)
+    answers = models.IntegerField('respostas', blank=True, default=0)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now_add=True)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'tópico'
+        verbose_name_plural = 'tópicos'
+        ordering = ['-updated_at']
+
+
+class Reply(models.Model):
+    reply = models.TextField('resposta')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='autor', related_name='replies'
+    )
+    correct = models.BooleanField('correta?', blank=True, default=False)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now_add=True)
+
+    def __str__(self):
+        return self.reply[:100]
+
+    class Meta:
+        verbose_name = 'resposta'
+        verbose_name_plural = 'respostas'
+        ordering = ['-correct', 'created_at']
