a8b173827ecacc277c2185e9e0fa2530f1160ced
Izabela Guerreiro <izaguerreiro@gmail.com>
2017-02-02 17:09:39 -0200

    Class Based View

--- simplemooc/simplemooc/threads/views.py
+++ simplemooc/simplemooc/threads/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View, ListView
+from .models import Thread
+
+#class ThreadView(View):
+#    def get(self, request, *args, **kwargs):
+#    	return render(request, 'threads/index.html')
 
 
-class ThreadView(TemplateView):
+class ThreadView(ListView):
+    model = Thread
+    paginate_by = 10
     template_name = 'threads/index.html'
 
 
+
 index = ThreadView.as_view()
-index = ThreadView.as_view(template_name='threads/index.html')
